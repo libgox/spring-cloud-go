53547b8e80c5650436c50c799027937e85ae967b
ZhangJian He <shoothzj@gmail.com>
2024-11-11 19:36:12 +0800

    feat: add field in endpoint (#32)
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- endpoint.go
+++ endpoint.go
@@ -1,11 +1,17 @@
 package springcloud
 
 type Endpoint struct {
-	Name                string `json:"name"`
-	Id                  string `json:"id"`
-	Address             string `json:"address"`
-	Port                int    `json:"port"`
-	SslPort             *int   `json:"sslPort"`
-	RegistrationTimeUTC int64  `json:"registrationTimeUTC"`
-	ServiceType         string `json:"serviceType"`
+	Name                string      `json:"name"`
+	Id                  string      `json:"id"`
+	Address             string      `json:"address"`
+	Port                int         `json:"port"`
+	SslPort             *int        `json:"sslPort"`
+	Payload             interface{} `json:"payload"`
+	RegistrationTimeUTC int64       `json:"registrationTimeUTC"`
+	ServiceType         string      `json:"serviceType"`
+	UriSpec             UriSpec     `json:"uriSpec"`
+}
+
+type UriSpec struct {
+	Parts map[string]bool `json:"parts"`
 }
